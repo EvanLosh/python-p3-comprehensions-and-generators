b558e0a65f3daf629fef571d232643357dcfcaab
Evan Losh <evand.losh@gmail.com>
2023-12-14 13:00:59 -0500

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if n % 2 == 0]
+    return evens
+    
 
 def make_exclamation(sentence_list):
-    pass
+    changed_sentences = [s + "!" for s in sentence_list ]
+    return changed_sentences
